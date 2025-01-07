fdfef2702096243f695efe9cf67a54c75fa1ad3e
Yordanos Temesgen <yorditjordin@gmail.com>
2025-01-07 21:42:30 +0300

    javascrip operations assignment1

---
+++ code/5-javascript-1/script.js
@@ -0,0 +1,14 @@
+// Strings
+const myName = "Yordanos Temesgen";
+console.log(myName.length);
+console.log(`Hello, my name is ${myName}`);
+console.log(`When my friends see me they shout ${myName.toUpperCase()}`);
+
+// Numbers
+const x = 5;
+const y = (x * 2 + 8) / 2 - x;
+console.log(y);
+
+const radies = 2;
+const area = Math.PI * radies ** radies;
+console.log("Area = " + area.toFixed(4));
