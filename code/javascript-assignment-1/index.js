a3820518984badae5a9b5ad36d8421f787627bde
Patutechz <patrickokpunor65@gmail.com>
2025-01-11 17:49:19 +0100

    added assignment assignment 1

---
+++ code/javascript-assignment-1/index.js
@@ -0,0 +1,26 @@
+//  Strings
+let myname = "Patrick"; 
+console.log(`My name is ${myname.length} letters long`);
+console.log(`Hello, my name is ${myname}`);
+console.log(`When my friends see me they shout ${myname.toUpperCase()}!`);
+console.log('');
+console.log('');
+
+// Numbers
+// Challenge 1:
+let myNumber = 4;
+myNumber * 2;
+myNumber + 8;
+myNumber / 2;
+myNumber - 4;
+console.log(`The result of this operation is ${myNumber}`);
+console.log('');
+console.log('');
+
+
+// Challenge 2:
+let pi = 3.1415926535897323846
+let radius = 3;
+area = pi * radius * radius;
+console.log(`The area of the circle is ${Math.round(area * 10000) / 10000}`)
+
