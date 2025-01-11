cec9fa20ad61f749c46adfefe70a83c6a086bc97
lucidshaya <ohinegames@gmail.com>
2025-01-11 11:40:38 +0100

    final html and css project

---
+++ javascript assignment 1/index.js
@@ -0,0 +1,24 @@
+// challenge 1
+const myName = "lucid"
+console.log(myName.length)
+console.log(`"Hello my name is ${myName}" `);
+console.log(`When my friends see me they shout ${myName.toUpperCase()}!`);
+
+// challenge 2 
+const   MyNumber = 10; // You can choose any number
+
+const result = ((MyNumber * 2 + 8) / 2) - MyNumber;
+
+console.log(result);
+
+// challenge 3 
+
+const radius = 2; 
+
+
+const area = Math.PI * Math.pow(radius, 2);
+
+
+const roundedArea = parseFloat(area.toFixed(4));
+
+console.log(roundedArea);
