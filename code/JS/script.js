d50b81b287a6aed93d788b3efa27e07c1699e7b5
FlorenceM <kekeletsomatabane@gmail.com>
2025-01-11 02:11:46 +0200

    add number operations

--- code/JS/script.js
+++ code/JS/script.js
@@ -5,3 +5,15 @@ console.log(myName.length);
 console.log(`Hello, my name is ${myName}.`);
 
 console.log(`When my friends see me they shout ${myName.toUpperCase()}!`);
+
+// number operations
+// challenge 1
+const eggs = 12;
+
+let newNum = ((((eggs * 2) + 8) / 2) - eggs);
+console.log(newNum);
+
+// challenge 2
+const radius = 10;
+const area = (Math.PI * (radius * radius)).toFixed(4);
+console.log(area);
