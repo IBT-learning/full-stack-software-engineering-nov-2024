d6c676868964c038304425bd0fee054a7d104bec
FlorenceM <kekeletsomatabane@gmail.com>
2025-01-11 02:04:14 +0200

    add string operations

---
+++ code/JS/script.js
@@ -0,0 +1,7 @@
+// string operations
+const myName = 'Florence';
+console.log(myName.length);
+
+console.log(`Hello, my name is ${myName}.`);
+
+console.log(`When my friends see me they shout ${myName.toUpperCase()}!`);
