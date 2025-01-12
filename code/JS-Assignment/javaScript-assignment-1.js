a34b48e914a5575817a78df29ac06c2f4bb132d3
Augustine Abah <ababaug@gmail.com>
2025-01-12 03:21:47 +0100

    solve the string assignment problem

---
+++ code/JS-Assignment/javaScript-assignment-1.js
@@ -0,0 +1,5 @@
+// Strings
+const myName = 'Augustine';
+console.log(myName.length);
+console.log(`Hello, my name is ${myName}`);
+console.log(`When my friends see me they shout ${myName.toUpperCase()}!`);
