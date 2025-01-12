c0c175168241387e20c5ecfc5fbcd8cab498b494
Augustine Abah <ababaug@gmail.com>
2025-01-12 03:42:34 +0100

    add challenge 1 and 2 solution

--- code/JS-Assignment/javaScript-assignment-1.js
+++ code/JS-Assignment/javaScript-assignment-1.js
@@ -3,3 +3,12 @@ const myName = 'Augustine';
 console.log(myName.length);
 console.log(`Hello, my name is ${myName}`);
 console.log(`When my friends see me they shout ${myName.toUpperCase()}!`);
+
+// Numbers - Challenge 1
+const num = 23;
+console.log((num * 2 + 8) / 2 - num);
+
+// Area of Circle - challenge 2
+const radius = 2;
+const circleArea = Math.PI * radius ** 2;
+console.log(`The area of circle = ${circleArea.toFixed(4)}`);
