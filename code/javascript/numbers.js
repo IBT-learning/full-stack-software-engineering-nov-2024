c819008155cbcbf500a15bd50ca4ecb5284a2a02
michael-lfc <michelino.realmike@gmail.com>
2025-01-13 22:25:05 +0100

    first commit

---
+++ code/javascript/numbers.js
@@ -0,0 +1,14 @@
+// Number Operations
+const num = 10
+
+const result = ((num * 2 + 8) / 2) - num
+
+console.log(result)
+
+//Area of circle
+
+const radius = 2
+
+const area = (Math.PI * Math.pow(radius, 2)).toFixed(4)
+
+console.log(area)
