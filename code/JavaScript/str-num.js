305746ce77841890fbe5f8bddb6f91fd70024b25
Esohnobhn <ifadafavour50@gmail.com>
2025-01-18 00:50:22 +0100

    My javascript first assignment

---
+++ code/JavaScript/str-num.js
@@ -0,0 +1,18 @@
+const myName = 'Techbro';
+console.log(myName.length)
+console.log('Hello, my name is ' + myName)
+console.log(`When my friends see me they shout ${myName.toUpperCase()}!`)
+
+
+
+// Number
+const number = 100
+let answer = (((number * 2)+8)/2)-number
+console.log (answer)
+
+
+const radius = 2
+const area = Math.PI * radius ** 2
+console.log(area.toFixed(4))
+
+
