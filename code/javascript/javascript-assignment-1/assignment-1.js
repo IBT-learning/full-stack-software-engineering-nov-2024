1e37183dfa891541c44892039f0435f9068c7a06
abigel <abigelameha@gmail.com>
2025-01-18 14:41:34 +0300

    javascript-assignment-1

---
+++ code/javascript/javascript-assignment-1/assignment-1.js
@@ -0,0 +1,22 @@
+//Strings
+
+const myName = "Abigel";
+
+    console.log(myName.length);
+    console.log(`Hello, my name is ${myName}`);
+    console.log(`When my friends see me they shout ${myName.toUpperCase()}!`);
+
+//Numbers
+//Challenge 1
+const Numbers = "10"
+      let result = Numbers * 2;
+      result += 8;
+      result /= 2;
+      result -= Numbers
+      console.log(result)
+
+//Challenge 2\
+const radius = "2"
+      let area = Math.PI * radius**2
+      console.log(area.toFixed(4))
+
