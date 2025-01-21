7b6dddc5cc74bb73e370aea59b8da537fcd47fc6
Yordanos Temesgen <yorditjordin@gmail.com>
2025-01-21 16:01:56 +0300

    did challenge 4

--- code/2-javascript/script4.js
+++ code/2-javascript/script4.js
@@ -1,4 +1,4 @@
-// Challenge 1
+// ## Challenge 1
 const wordList = [
   "every",
   "word",
@@ -9,19 +9,29 @@ const wordList = [
   "be",
   "capitalized",
 ];
-// for(const word of wordList) console.log(word.toUpperCase());
 // capitalize first letter
-// const newWordList = wordList.slice(0)
-// for(const word of newWordList){
-//     // let Word = word.slice(0,1).toUpperCase()+ word.slice(1,word.length)
-//     //use spliced
-//     const firstLetter = word.slice(0,1).toUpperCase();
-//     console.log(firstLetter)
-//     let word1 = newWordList.splice(0,1,firstLetter)
-//     console.log(newWordList)
-// }
+const newWordList = Array(wordList.length).fill("");
+let counter = 0;
+for (const word of wordList) {
+  const firstLetter = word.slice(0, 1).toUpperCase();
+  const newWord = firstLetter + word.slice(1);
+  newWordList[counter++] += newWord;
+}
+console.log(newWordList);
 
-console.log("newWordList".slice(0, 1));
-const tobeSpliced = ["hiii", "hey", "hiii", "hey"];
-console.log(tobeSpliced.splice(1, 1, "delete hey only"));
-console.log(tobeSpliced);
+// ## Challenge 2
+console.log("\n\n\t\t\t\t challenge 2");
+let i = 10;
+for (let i = 0; i < 10; i++) {
+  console.log(i, " - ", i % 3 == 0);
+}
+// Fizz Buzz
+let loops = 1;
+while (loops <= 40) {
+  if (loops % 3 == 0) {
+    if (loops % 5 == 0) console.log(`\t\t ${loops} = FizzBuzz`);
+    else console.log(`\t\t ${loops} = Fizz`);
+  } else if (loops % 5 == 0) console.log(`\t\t ${loops} = Buzz`);
+  else console.log('\t\t',loops)
+  loops += 1;
+}
