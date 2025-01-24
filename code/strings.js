6d393d2b6b54d4cd1ead06447838b66348a000ab
jacklynmwas <mwasjacky15@gmail.com>
2025-01-24 07:46:38 +0300

    move code/strings.js to git folder

---
+++ code/strings.js
@@ -0,0 +1,23 @@
+// strings assignment
+
+const me = "Jackline"
+ console.log(me)
+ console.log(me.length)
+ const greet = "Hello,my name is"
+  console.log('Hello, my name is' + " " + me)
+ const people = " When my friends see me they shout" 
+ console.log('When my friends see me they shout' + " " + me.toUpperCase() + '!')
+
+//  numbers assignment 1
+ const one = 13
+  console.log((13*2+8)/2-13)
+
+// numbers assignment 2
+function calculateArea(radius) {
+    const area = Math.PI * Math.pow(radius, 2);
+    console.log(area.toFixed(4)); 
+}
+ calculateArea(3);
+
+
+
