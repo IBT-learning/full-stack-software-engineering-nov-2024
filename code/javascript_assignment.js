7bcf3cddd07738ec0b7d8c30c3dcc9558d94e9c5
sollyboy <sollybo1@gmail.com>
2025-01-25 02:07:36 +0100

    javascript assignment

---
+++ code/javascript_assignment.js
@@ -0,0 +1,27 @@
+const myName = "Solomon";
+console.log(`My name is ${myName.length} letters long`);
+console.log(`Hello, my name is ${myName}`);
+console.log(`When my friends see me they shout ${myName.toUpperCase()}!`);
+console.log('');
+console.log('');
+
+//Number
+//Challenge 1
+let theNumber = 5;
+theNumber = theNumber * 2;
+theNumber = theNumber + 8;
+theNumber = theNumber / 2;
+theNumber = theNumber - 5;
+console.log(`the result is ${theNumber}`);
+
+
+
+
+
+
+
+// Challenge 2:
+let pi = 3.1415926535897323846
+let radius = 3;
+area = pi * radius * radius;
+console.log(`The area of the circle is ${Math.round(area * 10000) / 10000}`)
