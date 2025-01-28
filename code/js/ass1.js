8e073d88770f8af51aab6ae86a1ce6cf4fe62d75
olawalesegun <olawalesegun0@gmail.com>
2025-01-28 05:05:57 +0300

    add:: js task 1 added and implemented

--- code/js/ass1.js
+++ code/js/ass1.js
@@ -4,3 +4,14 @@ result += 8;
 result /= 2;
 result -= var1;
 console.log(result);
+
+
+console.log("-----------")
+
+const pie = 22/7
+result = pie * 2 * 2
+console.log(pie)
+console.log(`Result for Radius 2::`, result.toPrecision(6))
+
+result = pie * 3 * 3
+console.log(`Result for Radius 3::`, result.toPrecision(6))
