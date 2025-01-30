b9e5c90c8cbbe8e581e43d448ed156772789df83
FlorenceM <kekeletsomatabane@gmail.com>
2025-01-30 02:32:17 +0200

    add challenge 3 code

--- code/JS/JS-assignment5/script5.js
+++ code/JS/JS-assignment5/script5.js
@@ -11,3 +11,15 @@ function perentCalc(amount, percentage){
 }
 
 console.log(perentCalc(200, 20));
+
+// Challenge 3
+function divisible (divided, divisor){
+  if(divided % divisor === 0){
+    return true;
+  }else{
+    return false;
+  }
+}
+
+console.log(divisible(10, 2));
+console.log(divisible(15, 6));
