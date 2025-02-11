cc5029027939f640da2df3b374685cf07e8aa9fd
FlorenceM <kekeletsomatabane@gmail.com>
2025-02-11 23:36:23 +0200

    add challenge 2 and 3 code

--- code/JS/JS-assignment8/script8.js
+++ code/JS/JS-assignment8/script8.js
@@ -12,3 +12,28 @@ console.log(evenNums);
 
 let divisibleBy3 = nums.filter((element) => element % 3 === 0);
 console.log(divisibleBy3);
+
+// Challenge 2
+let square = nums.map((element) => element * element);
+console.log(square);
+
+let half = nums.map((element) => element / 2);
+console.log(half);
+
+// Challenge 3
+const prices = [
+  { product: "shoes", price: 50, inStock: true },
+  { product: "light bulb", price: 3, inStock: true },
+  { product: "stuffed animal", price: 15, inStock: false },
+  { product: "jacket", price: 75, inStock: false },
+  { product: "keychain", price: 4, inStock: true },
+];
+
+let lessThan20 = prices.filter((element) => element.price < 20);
+console.log(lessThan20);
+
+let inStockProd = prices.filter((element) => element.inStock);
+console.log(inStockProd);
+
+let faveProd = prices.filter((element) => element.price < 20 && element.inStock);
+console.log(faveProd);
