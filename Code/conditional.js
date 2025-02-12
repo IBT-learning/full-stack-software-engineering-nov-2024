cfe35c37dfab2b48469016169877a5149aa71964
Daud Akanji <akanjidaud2@gmail.com>
2025-02-12 12:42:01 -0800

    booleans and conditional assignment

---
+++ Code/conditional.js
@@ -0,0 +1,34 @@
+//let animal = 25
+//let sheep = true
+//let goat = false
+
+
+//if (animal < 35 && sheep) {
+  //  console.log('There is a surplus animal');
+//} else if (!sheep) {
+    //console.log('There will be no surplus animal');  
+//} else if (animal != goat && sheep < animal) {
+
+
+
+//CHALLENGE: CONDITIONAL
+
+const movieTitle = 'Terminator'
+console.log(movieTitle);
+const isScary = true
+console.log(isScary);
+const isRomantic = false
+console.log(isRomantic);
+
+if (isScary && isRomantic) {
+  console.log('Terminator is both romantic and scary');
+} else if (isScary) {
+  console.log('Terminator is not romantic');  
+} else if (isRomantic) {
+  console.log('Terminator is romantic');  
+} else {
+  console.log('Terminator is neither romantc nor scary');  
+}
+
+
+
