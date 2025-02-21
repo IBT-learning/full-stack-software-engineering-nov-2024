16fb6b3f3a543b2edd50159d110b85d02019cde2
Yordanos Temesgen <yorditjordin@gmail.com>
2025-02-21 10:25:32 +0300

    hello

--- code/2-javascript/script7.js
+++ code/2-javascript/script7.js
@@ -1,14 +1,31 @@
-// 
-
+// Challenge 1
 class BankAccount {
-    constructor(ownerName, balance) {
-    ownerName: ownerName;
-    balance: balance;
+  constructor(ownerName, balance) {
+    this.ownerName = ownerName;
+    this.balance = balance || 0;
+  }
+  // Challenge 2
+  deposit(amount) {
+    return typeof amount === "number" && (this.balance += amount);
+  }
+
+  // Challenge 3
+  withdraw = (amount) =>
+    (typeof amount === "number" &&
+      (amount <= this.balance) &&
+      (this.balance -= amount)) ||
+    (!typeof amount === "number" && console.log(amount, " Not a number !")) ||
+    console.log("Insufficient funds");
 }
-ownerName= 'default';
-balance = 0;
-}
-console.log('our class -', BankAccount, 'ouwner name ', BankAccount.ownerName, 'balance ', BankAccount.balance)
-console.log(' constructed inside console - ', new BankAccount('John Doe', 1000), "owner name = ", new BankAccount('John Doe', 1000).ownerName, 'balance = ', new BankAccount('John Doe', 1000).balance)
-let account = new BankAccount('John Doe', 1000);
-console.log(' constructed class - ', account, "owner name ", account.ownerName, 'balance ', account.balance)
+const person = new BankAccount("John Doe");
+console.log("class", person);
+
+let x = 100;
+let retD = person.deposit(x);
+console.log("deposit, ", 100, "  balance = ", person.balance, "return -", retD);
+
+let y = 2;
+let retW = person.withdraw(y);
+console.log(" withdraw  ", y, " balance = ", person.balance, "withdraw returns -", retW);
+
+console.log(person.toConosle(2))
