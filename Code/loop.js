6d14d8299ceb21f6f398ccc1a7164392dda98626
Daud Akanji <akanjidaud2@gmail.com>
2025-03-06 06:36:02 -0800

    loop assignment

--- Code/loop.js
+++ Code/loop.js
@@ -1,3 +1,5 @@
+//Challenge 1
+
 const wordList = ["every", "word", "in", "this", "array", "should", "be", "capitalized"]
 const capWordList = [];
 
@@ -14,3 +16,19 @@ capWordList.push(capitalized);
 }
 
 console.log(capWordList);
+
+// Challenge 2
+
+const num1 = [1,2,,3,4];
+const num2 = [5,6,7,8,9,10];
+
+const concatNum = num1.concat(num2);
+console.log(concatNum);
+
+let num = 0
+while (num <= 10) {
+    console.log("The nmber is true:", num);
+    num++;
+
+}
+
