f92dd680e75c1bcb86c71cdacd1a635c448ec793
zaktayyip <zakidayib1997@gmail.com>
2025-03-14 14:52:25 +0300

    solve web api assigment 1

---
+++ code/web app assigm1/index.js
@@ -0,0 +1,27 @@
+
+
+let banner = document.getElementById("banner");
+let darkEl = document.getElementById("dark-btn")
+let lightEl = document.getElementById("light-btn");
+
+// console.log(banner)
+// console.log(darkEl)
+// console.log(lightEl)
+
+darkEl.addEventListener("click", function(){
+    banner.textContent = "Dark Mode";
+    banner.style.color = "white";
+    banner.style.backgroundColor = "black";
+    banner.style.width = "200px";
+    banner.style.textAlign = "center";
+    
+})
+
+lightEl.addEventListener("click", function(){
+    banner.textContent = "light Mode";
+    banner.style.color = "black";
+    banner.style.backgroundColor = "tomato";
+    banner.style.width = "200px";
+    banner.style.textAlign = "center";
+    
+})
