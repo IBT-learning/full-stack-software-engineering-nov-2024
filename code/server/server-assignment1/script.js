752da0fdeea1e1899d59be989a616a15da639f51
FlorenceM <kekeletsomatabane@gmail.com>
2025-03-27 02:17:46 +0200

    init commit server assignment1

---
+++ code/server/server-assignment1/script.js
@@ -0,0 +1,7 @@
+import chalk from 'chalk';
+
+console.log(chalk.bold('Here is some bold text!'));
+console.log(chalk.green('Over here is some green text!'));
+console.log(chalk.bgBlack('This one has a black background.'));
+console.log(chalk.italic('This is some italic text'));
+console.log(chalk.bgWhite.red('this text combines styling: a background of white and red text'));
