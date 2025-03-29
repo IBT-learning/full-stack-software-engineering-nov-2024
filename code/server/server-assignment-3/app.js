13d513342edad7a9d748c21e131f30d52ed967b9
Danny Burrow <burrowingdan@gmail.com>
2025-03-29 10:24:29 -0700

    remove non-working endpoints

--- code/server/server-assignment-3/app.js
+++ code/server/server-assignment-3/app.js
@@ -1,34 +1,21 @@
-import express from 'express';
+import express from "express"
 
-const app = express();
-app.use(express.json());
+const app = express()
+app.use(express.json())
 
-app.get('https://jsonplaceholder.typicode.com/users', (req, res) => {
-    const data = res.json();
-    // res.send(`Got ${data.length} users`);
-});
-
-app.get('https://jsonplaceholder.typicode.com/users/5', (req, res) => {
-    const data = res.json();
+app.get("/", (req, res) => {
+  res.send("Hello you called the GET endpoint!")
 })
-
-app.post('https://jsonplaceholder.typicode.com/users', (req, res) => {
-    const data = res.json();
+app.put("/", (req, res) => {
+  res.send("Hello you called the PUT endpoint!")
 })
-
-app.get('/', (req, res) => {
-    res.send("Hello you called the GET endpoint!");
+app.post("/", (req, res) => {
+  res.send("Hello you called the POST endpoint!")
 })
-app.put('/', (req, res) => {
-    res.send("Hello you called the PUT endpoint!");
-})
-app.post('/', (req, res) => {
-    res.send("Hello you called the POST endpoint!");
-})
-app.delete('/', (req, res) => {
-    res.send("Hello you called the DELETE endpoint!");
+app.delete("/", (req, res) => {
+  res.send("Hello you called the DELETE endpoint!")
 })
 
 app.listen(4000, () => {
-    console.log('Server listening on port 4000!');
+  console.log("Server listening on port 4000!")
 })
