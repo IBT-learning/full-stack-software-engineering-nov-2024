286bcc94260522907a093b1bea5e204ed7fc2eeb
sherifah <sherifahhassan@gmail.com>
2025-03-31 07:20:14 +0100

    Server Assignment #1: npm practice

---
+++ my-chalk-app/app.js
@@ -0,0 +1,7 @@
+import chalk from "chalk";
+
+console.log(chalk.bold("Bold Text"));
+console.log(chalk.blue("Blue Text"));
+console.log(chalk.greenBright("Bright Green Text"));
+console.log(chalk.bgRed.white("White on Red Background"));
+console.log(chalk.underline.magenta("Underlined Magenta Text"));
