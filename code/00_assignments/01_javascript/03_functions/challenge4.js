59bf08a239fce6fde138c78723df2a18d3943d72
Mareola <bulun1001@gmail.com>
2025-04-03 16:02:02 +0100

    feat(file): add challenge4.js

---
+++ code/00_assignments/01_javascript/03_functions/challenge4.js
@@ -0,0 +1,25 @@
+// Shorten Console.log
+const cl = console.log;
+
+// Challenge #4: Friend, Enemy
+
+// Declare a function called greeting that takes in two arguments, firstName and status
+function greeting (firstName, status) {
+    
+    // Within the function, check if the status is "friend"
+    if (status === "friend") {
+        // If true, return a a greeting
+        return `Hello ${firstName}, my friend!`;
+    } 
+    else if (status == "enemy"){
+        // If false, tell them to go awaay
+        return `Hello ${firstName}, Please go away!`;
+    }
+    else{
+        // Extra Challenge
+        return `Hello ${firstName}, Sorry, I don't know you`;
+    }
+}
+
+// Test the function with different names and statuses
+cl(greeting("John", "friend"));
