cdb48347835ac5213db8feaedaa8a26a0bda9120
FlorenceM <kekeletsomatabane@gmail.com>
2025-04-08 05:29:09 +0200

    add code for userdata

--- code/WebAPIs/WebAPI-final-project/script.js
+++ code/WebAPIs/WebAPI-final-project/script.js
@@ -5,17 +5,23 @@ async function fetchPosts(){
     const responseObj = await response.json(); 
     console.log(responseObj);
     
-    const appPosts = document.querySelectorAll('.text'); 
+    const appPosts = document.querySelectorAll('.post'); 
     const postsLength = appPosts.length;
-    
   
     for (let i = 0; i < postsLength; i++){
-      appPosts[i].textContent = responseObj[i].body;
+      // to change the post content
+      const textSection = appPosts[i].querySelector('.text');
+      textSection.textContent = responseObj[i].body;
 
-
+      // to change the username
+      const userName = await fetch(`https://jsonplaceholder.typicode.com/users/${(i+1)}`);
+      const userNameObj = await userName.json();
+  
+      const nameSpan = appPosts[i].querySelector('.name');
+      nameSpan.textContent = userNameObj.username;
     }
   }catch(error){
-    console.error('Error:', error);
+    console.error('Failed to fetch posts:', error);
   }
 }
 
