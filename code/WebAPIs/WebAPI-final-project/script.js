d3e69ed76d404eeecac70ac1f1add760639b4fd0
FlorenceM <kekeletsomatabane@gmail.com>
2025-04-25 01:16:24 +0200

    final version

--- code/WebAPIs/WebAPI-final-project/script.js
+++ code/WebAPIs/WebAPI-final-project/script.js
@@ -1,26 +1,22 @@
-async function fetchPosts(){
-  try{
-    const response = await fetch(`https://jsonplaceholder.typicode.com/posts`); 
-    
-    const responseObj = await response.json(); 
-    console.log(responseObj);
-    
-    const appPosts = document.querySelectorAll('.post'); 
-    const postsLength = appPosts.length;
-  
-    for (let i = 0; i < postsLength; i++){
-      // to change the post content
-      const textSection = appPosts[i].querySelector('.text');
-      textSection.textContent = responseObj[i].body;
+async function fetchPosts() {
+  try {
+    const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+    const posts = await response.json();
 
-      // to change the username
-      const userName = await fetch(`https://jsonplaceholder.typicode.com/users/${(i+1)}`);
-      const userNameObj = await userName.json();
-  
-      const nameSpan = appPosts[i].querySelector('.name');
-      nameSpan.textContent = userNameObj.username;
+    const appPosts = document.querySelectorAll('.post');
+    const postsLength = appPosts.length;
+
+    for (let i = 0; i < postsLength; i++) {
+      const post = posts[i];
+      console.log(post);
+      
+      if (!post) continue;
+
+      // to change post content
+      const textSection = appPosts[i].querySelector('.text');
+      textSection.textContent = post.body;
     }
-  }catch(error){
+  } catch (error) {
     console.error('Failed to fetch posts:', error);
   }
 }
