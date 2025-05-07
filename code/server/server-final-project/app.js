8a273818a08b4a215004272ebe73172f198eb50e
FlorenceM <kekeletsomatabane@gmail.com>
2025-05-07 04:19:47 +0200

    add final version

--- code/server/server-final-project/app.js
+++ code/server/server-final-project/app.js
@@ -1,26 +1,18 @@
 import express from 'express';
 import path from 'path';
-import fs from 'fs';
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.static('public'));
 
-// for all the posts
-app.get('/posts', (req, res) => {
+app.get('/api/posts', (req, res) => {
   res.sendFile(path.join(import.meta.dirname, 'public/blogData.json'));
 });
 
-//
-app.get('/posts/:id', (req, res) => {
-  const id = req.params.id;
-
-  fs.readFile(path.join(import.meta.dirname, 'public/blogData.json'));
-  console.log(id);
-
-  // const post = 
-});
+app.get('/posts', (req, res) => {
+  res.sendFile(path.join(import.meta.dirname, 'public/index.html'));
+})
 
 app.listen(PORT, () => {
   console.log(`Listening on 127.0.0.1:${PORT}`);
