4d8a91e9fc92e638c1258ffb8628ff8d4066f449
michael-lfc <michelino.realmike@gmail.com>
2025-05-11 01:01:04 +0100

    initial commit

---
+++ code/michael-react-assignment-1/mood-board/src/App.jsx
@@ -0,0 +1,14 @@
+import MoodBoard from "./MoodBoard";
+
+function App() {
+  return (
+    <>
+      <MoodBoard name="Ani" />
+      <MoodBoard name="Ayo" />
+      <MoodBoard name="Michael" />   
+      <MoodBoard name="Samuel" />
+    </>
+  );
+}
+
+export default App;
