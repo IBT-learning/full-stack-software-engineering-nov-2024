bf33ebe9b3bf135fe9ace73dcdf2db1b64db5df2
FlorenceM <kekeletsomatabane@gmail.com>
2025-05-12 07:38:48 +0200

    add code for get by id

--- code/Database/Database-assignment2/controllers/recipes.js
+++ code/Database/Database-assignment2/controllers/recipes.js
@@ -15,4 +15,23 @@ router.get('/', async (req, res) => {
   }
 });
 
+router.get('/find/:recipeId', async (req, res) => {
+  const id = req.params.recipeId;
+  try{
+    if (!mongoose.Types.ObjectId.isValid(id)){
+      return res.status(404).json({'Error': 'Invalid recipe Id'});
+    }
+
+    const recipe = await Recipe.findById(id);
+    if (!recipe){
+      return res.status(404).json({'Error': 'Recipe not found'});
+    }
+
+    res.json(recipe);
+  }catch (err) {
+    console.error('Error: ', err);
+    res.status(500).json({'Error': 'Cannot retrieve recipe'});
+  }
+});
+
 export default router;
