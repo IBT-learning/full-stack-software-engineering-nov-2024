afa4b5d4da7fa541ed64ad202f0e66e2f577c05e
FlorenceM <kekeletsomatabane@gmail.com>
2025-05-13 00:08:18 +0200

    add code for post, put and delete endpoints

--- code/Database/Database-assignment2/controllers/recipes.js
+++ code/Database/Database-assignment2/controllers/recipes.js
@@ -24,14 +24,83 @@ router.get('/find/:recipeId', async (req, res) => {
 
     const recipe = await Recipe.findById(id);
     if (!recipe){
-      return res.status(404).json({'Error': 'Recipe not found'});
+      return res.status(404).json({'Error': 'Requested recipe not found'});
     }
 
     res.json(recipe);
   }catch (err) {
-    console.error('Error: ', err);
+    console.error('Error finding recipe: ', err);
     res.status(500).json({'Error': 'Cannot retrieve recipe'});
   }
 });
+
+router.post('/', async (req, res) => {
+  try{
+    const newRecipe = new Recipe(req.body);
+    await newRecipe.save();
+    res.status(201).json(newRecipe);
+  }catch (err){
+    console.error('Error creating recipe: ', err);
+
+    // for Mongoose validation errors
+    if(err.name === 'ValidationError'){
+       return res.status(422).json({
+        'Error': 'Validation failed',
+        'Details': err.errors
+      });
+    }
+
+    res.status(422).json({'Error': 'Failed to create recipe'});
+  }
+});
+
+router.put('/update/:recipeId', async (req, res) => {
+  const id = req.params.recipeId;
+  try{
+    if (!mongoose.Types.ObjectId.isValid(id)){
+      return res.status(422).json({'Error': 'Invalid recipe Id'});
+    }
+
+    const result = await Recipe.updateOne({_id: id}, req.body);
+    console.log(result);
+    if (result.matchedCount === 0){
+      return res.status(404).json({'Error': 'Recipe not found'});
+    }
+
+    res.status(200).json({'Success': 'Updated a recipe'});
+  }catch (err) {
+    console.error('Error updating recipe: ', err);
+
+    // for Mongoose validation errors
+    if(err.name === 'ValidationError'){
+       return res.status(422).json({
+        'Error': 'Validation failed',
+        'Details': err.errors
+      });
+    }
+
+    res.status(500).json({'Error': 'Failed to update selected recipe'});
+  }
+});
+
+router.delete('/delete/:recipeId', async (req, res) => {
+  const id = req.params.recipeId;
+  try{
+     if (!mongoose.Types.ObjectId.isValid(id)){
+      return res.status(422).json({'Error': 'Invalid recipe Id'});
+    }
+
+    const result = await Recipe.deleteOne({_id: id});
+    // console.log(result);
+    if (result.deletedCount === 0){
+      return res.status(404).json({'Error': 'Recipe not found'});
+    }
+
+    res.status(200).json({'Success': 'Deleted recipe'});
+  }catch (err) {
+    console.error('Error deleting recipe: ', err);
+    res.status(500).json({'Error': 'Failed to delete recipe'});
+  }
+}); 
 
 export default router;
