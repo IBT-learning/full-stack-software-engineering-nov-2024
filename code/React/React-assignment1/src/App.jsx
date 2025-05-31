0adb40e7d452267a91da0218e9d1d267b6317585
FlorenceM <160511795+kekeletsomatabane@users.noreply.github.com>
2025-05-31 14:07:22 +0200

    React assignment 1  (#486)
    * init commit
    * add extra challenge code

---
+++ code/React/React-assignment1/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+import MoodComp from './components/Mood.jsx';
+
+function App(){
+
+  return (
+    <>
+    <MoodComp name='Florence'/>
+    <MoodComp name='Usain'/>
+    <MoodComp name='Katleho'/>
+    </>
+  );
+}
+
+export default App;
