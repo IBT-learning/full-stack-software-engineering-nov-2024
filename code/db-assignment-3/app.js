e50ced91432140b1fa2b08bb416e4aa4f7fe5e7e
michael-lfc <michelino.realmike@gmail.com>
2025-06-07 15:31:41 +0100

    modified app.js

---
+++ code/db-assignment-3/app.js
@@ -0,0 +1,28 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import { dbConnect } from "./db.js";
+import recipeRouter from "./controllers/recipes.js";
+import userRouter from "./controllers/users.js";
+import tokenValidation from "./middlewares/tokenValidation.js"
+
+const app = express();
+dotenv.config();
+const PORT = process.env.PORT;
+
+// Middleware
+app.use(express.json());
+app.use(cors());
+
+app.use("/users", userRouter); 
+// app.use("/recipes", tokenValidation, recipeRouter)
+
+app.use("/recipes", recipeRouter)
+
+
+// Start the server
+app.listen(PORT, () => {
+  dbConnect();
+  console.log(`[server]: listening on port ${PORT}`);
+});
+
