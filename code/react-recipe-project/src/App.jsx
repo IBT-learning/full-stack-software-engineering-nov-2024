87ff32a6f18b59a72da73b213d7150d2f1ce3f10
michael-lfc <michelino.realmike@gmail.com>
2025-06-15 13:03:06 +0100

    modified:   src/App.jsx         modified:   src/components/Layout.jsx

--- code/react-recipe-project/src/App.jsx
+++ code/react-recipe-project/src/App.jsx
@@ -1,24 +1,64 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Layout from "./components/Layout";
-import Home from "./pages/Home";
-import CreatePage from "./pages/CreatePage";
+// import React from "react";
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import Layout from "./components/Layout";
+// import Home from "./pages/Home";
+// import CreatePage from "./pages/CreatePage";
+// import RecipePage from "./pages/RecipePage";
+// import EditPage from "./pages/EditPage";
+// import './App.css';
+
+
+// const App = () => (
+//   <Router>
+//     <Layout>
+//       <Routes>
+//         <Route path="/" element={<Home />} />
+//         <Route path="/recipes" element={<RecipePage />} />
+//         <Route path="/create" element={<CreatePage />} />
+//         <Route path="/edit/:id" element={<EditPage />} />
+//       </Routes>
+//     </Layout>
+//   </Router>
+// );
+
+// export default App;
+
+// App.jsx
+
+import "./App.css"
+
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom"
+
+// import AppLayout from "./layouts/AppLayout"
+import AppLayout from "./components/Layout";
+
+import Home from "./pages/Home"
+import CreatePage from "./pages/CreatePage"
+import EditPage from "./pages/EditPage"
 import RecipePage from "./pages/RecipePage";
-import EditPage from "./pages/EditPage";
-import './App.css';
 
+// Define routes with createBrowserRouter
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<AppLayout/>}>
+      <Route index element={<Home />} />
+      {/* Home page (list of recipes) */}
+      <Route path="/recipes" element={<RecipePage />} />
+      {/* Create page to add a new recipe */}
+      <Route path="create" element={<CreatePage />} />
+      {/* Edit page to edit existing recipe by id */}
+      <Route path="edit/:id" element={<EditPage />} />
+    </Route>
+  )
+)
 
-const App = () => (
-  <Router>
-    <Layout>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/recipes" element={<RecipePage />} />
-        <Route path="/create" element={<CreatePage />} />
-        <Route path="/edit/:id" element={<EditPage />} />
-      </Routes>
-    </Layout>
-  </Router>
-);
+function App() {
+  return <RouterProvider router={router} />
+}
 
-export default App;
+export default App
