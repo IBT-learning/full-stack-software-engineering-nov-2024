ed91e658de74c8d0b79afc73fcec7228b4fa3dcc
Daud Akanji <akanjidaud2@gmail.com>
2025-06-24 02:50:49 -0700

    server4Todo-Assignment

---
+++ Code/server2-express/public/index.js
@@ -0,0 +1,13 @@
+console.log("Identity");
+
+const image1 = document.getElementById("image1")
+    image1.style.border = "5px solid white"
+    image1.style.padding = "3px"
+
+
+const image2 = document.getElementById("image2")
+    image2.style.border = "5px solid white"
+    image2.style.padding = "5px"
+
+
+
