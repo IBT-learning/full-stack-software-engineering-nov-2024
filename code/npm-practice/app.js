ab0892ed9fce1ef8774376207f17166c3dd18722
h1 <henryoogun@gmail.com>
2025-08-06 14:38:43 +0100

    henryogun-npm-practice

---
+++ code/npm-practice/app.js
@@ -0,0 +1,7 @@
+import chalk from "chalk";
+
+console.log("Hello, this is my first npm project!");
+console.log(chalk.bold("hello"));
+console.log(chalk.red("This text is red!"));
+console.log(chalk.blue.underline("This text is blue and underlined!"));
+console.log(chalk.green.bold("This text is green and bold!"));
